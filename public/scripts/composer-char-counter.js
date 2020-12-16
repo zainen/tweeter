4a81ca8d69a3684a9d3fb55229253cd47b564727
zainen <zainensuzuki@gmail.com>
2020-12-16 00:33:45 +0000

    counter colors && function needs refactor later

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
-$(document).ready(() => {
-  console.log('My Body Is Ready')
-})
+$(() => {
+  const $textArea = $('#tweet-text');
+  const updateCount = () => {
+    // console.log($textArea.val())
+    const currentCount = 140 - $textArea.val().length;
+    const $counter = $('.counter');
+    $counter.text(currentCount);
+    if (currentCount < 0) {
+      $counter.addClass('red');
+    } else {
+      $counter.removeClass('red');
+    }
+  }
+  $textArea.on('keyup', updateCount);
+});
+
+// $('textarea').on('keyup', function() {
+//   const currentCount
+// })
